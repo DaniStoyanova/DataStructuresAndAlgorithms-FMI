eba2d13408731fd1935014299ad33b96d799ac99
Yordanka Stoyanova <75676911+DaniStoyanova@users.noreply.github.com>
2021-10-28 00:53:26 +0300

    Create mergeSortForDynamicArr.cpp

---
+++ Sorts/Merge Sort/mergeSortForDynamicArr.cpp
@@ -0,0 +1,52 @@
+void merge(int array[], int const left, int const mid, int const right)
+{
+    int const first_arr = mid - left + 1;
+    int const second_arr = right - mid;
+
+    int* left_arr = new int[first_arr];
+    int* right_arr = new int[second_arr];
+
+    for (int i = 0; i < first_arr; i++)
+        left_arr[i] = array[left + i];
+    for (int j = 0; j < second_arr; j++)
+        right_arr[j] = array[mid + 1 + j];
+
+    int index_first_arr = 0;
+    int index_second_arr = 0;
+    int index_merged_arr = left;
+
+    while (index_first_arr < first_arr && index_second_arr < second_arr) {
+        if (left_arr[index_first_arr] <= right_arr[index_second_arr]) {
+            array[index_merged_arr] = left_arr[index_first_arr];
+            index_first_arr++;
+        }
+        else {
+            array[index_merged_arr] = right_arr[index_second_arr];
+            index_second_arr++;
+        }
+        index_merged_arr++;
+    }
+
+    while (index_first_arr < first_arr) {
+        array[index_merged_arr] = left_arr[index_first_arr];
+        index_first_arr++;
+        index_merged_arr++;
+    }
+
+    while (index_second_arr < second_arr) {
+        array[index_merged_arr] = right_arr[index_second_arr];
+        index_second_arr++;
+        index_merged_arr++;
+    }
+}
+
+void mergeSort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+
+    int mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
