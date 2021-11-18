ce6fa356cf7ce28c0ae207f22c1773c13e7ac36b
Yordanka Stoyanova <75676911+DaniStoyanova@users.noreply.github.com>
2021-11-18 21:21:03 +0200

    Update Solution.cpp

--- Homeworks/Homework №6/No K-th/Solution.cpp
+++ Homeworks/Homework №6/No K-th/Solution.cpp
@@ -3,63 +3,150 @@
 #include <algorithm>
 #include <queue>
 
-using namespace std;
+using namespace std; 
+
+class BST
+{
+    int data;
+    BST* left, * right;
+
+public:
+    BST();
+    BST(int);
+
+    BST* insert(BST*, int);
+    BST* minValueNode(BST* node);
+    BST* deleteNode(BST* root, int key);
+
+    void printLevelOrder(BST* root);
+};
+
+BST::BST(): data(0), left(nullptr), right(nullptr){}
+
+BST::BST(int value)
+{
+    data = value;
+    left =  nullptr;
+    right = nullptr; 
+}
+
+BST* BST::insert(BST* root, int value)
+{
+    if (!root)
+    {
+        return new BST(value);
+    }
+
+    if (value < root->data)
+    {
+        root->left = insert(root->left, value);
+    }
+    else
+    {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+BST* BST::minValueNode(BST* node)
+{
+    BST* current = node;
+
+    while (current != nullptr && current->left != nullptr)
+        current = current->left;
+
+    return current;
+}
+
+BST* BST::deleteNode(BST* root, int data)
+{
+    if (root == nullptr)
+        return root;
+
+    if (data < root->data)
+        root->left = deleteNode(root->left, data);
+    else if (data > root->data)
+        root->right = deleteNode(root->right, data);
+    else 
+    {
+        if (root->left == nullptr and root->right == nullptr)
+            return nullptr;
+        else if (root->right == nullptr)
+        {
+            BST* temp = root->left;
+            free(root);
+            return temp;
+        }
+        else if (root->left == nullptr)
+        {
+            BST* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else
+        {
+            BST* temp = minValueNode(root->right);
+
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+    return root;
+}
+
+void BST::printLevelOrder(BST* root)
+{
+    if (root == nullptr) return;
+
+    queue<BST*> p;
+    p.push(root);
+
+    while (p.empty() == false)
+    {
+        int counter = p.size();
+
+        while (counter > 0)
+        {
+            BST* node = p.front();
+            cout << node->data << " ";
+            p.pop();
+            if (node->left != nullptr)
+                p.push(node->left);
+            if (node->right != nullptr)
+                p.push(node->right);
+            counter--;
+        }
+    }
+}
+
 
 int main()
 {
-    int N;
-    cin >> N;
-    
-    vector<pair<int, int>> numbers(N);
-    numbers[0].first = 0;
-    numbers[0].second = 0;
-    for (int i = 0; i < N; i++)
+    int N, K; 
+    cin >> N >> K; 
+
+    int num; 
+    cin >> num; 
+
+    BST b; 
+    BST* root = nullptr;
+    root = b.insert(root, num);
+
+    vector<int> numbers; 
+    numbers.push_back(num);
+
+    for (int i = 0; i < N - 1; i++)
     {
-        int num1, num2;
-        cin >> num1 >> num2;
-        if (num1 != -1)
-        {
-            numbers[num1].first = numbers[i].first - 1;
-            numbers[num1].second = numbers[i].second - 1;
-        }
-        if (num2 != -1)
-        {
-            numbers[num2].first = numbers[i].first + 1;
-            numbers[num2].second = numbers[i].second - 1;
-        }
+        int num1; 
+        cin >> num1; 
+        b.insert(root, num1);
+        numbers.push_back(num1);
     }
 
-    vector<pair<int, int>> new_vec;
-
-    for (int i = 0; i < N; i++)
-    {
-        bool hasDuplicate = false;
-        for (size_t j = 0; j < new_vec.size(); j++)
-        {
-            if (numbers[i].first == new_vec[j].second)
-            {
-                hasDuplicate = true;
-                break;
-            }
-        }
-
-        if (hasDuplicate == false)
-        {
-            pair<int, int> pair1;
-            pair1.second = numbers[i].first;
-            pair1.first = i;
-            new_vec.push_back(pair1);
-        }
-    }
-
-    std::sort(new_vec.begin(), new_vec.end(), [](auto& left, auto& right) {
-            return left.second < right.second;
-        });
-
-    for (size_t i = 0; i < new_vec.size(); i++)
-    {
-        cout << new_vec[i].first << " ";
-    }
+    sort(numbers.begin(), numbers.end());
+    b.deleteNode(root, numbers[N - K]);
+    b.printLevelOrder(root);
 
     return 0;
 }
